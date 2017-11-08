c1aa865688d53e6b0eb1ebc1f012c918ae54d178
Jure <jure.hotujec@gmail.com>
2017-11-08 11:57:47 +0100

    updates

--- crispy_forms_bulma/settings.py
+++ crispy_forms_bulma/settings.py
@@ -1,12 +1,5 @@
 #: -*- coding: utf-8 -*-
-"""
-Settings
-========
-Default required settings. You can override them in your project settings
-file.
-"""
 
-# Allowed layout pack
 CRISPY_ALLOWED_TEMPLATE_PACKS = (
     'bootstrap',
     'uni_form',
