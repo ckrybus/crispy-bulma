e8290bf5a6067a7162ec971feb7594e2af5d4ca8
Gareth Coles <gdude2002@gmail.com>
2019-02-26 12:49:16 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-crispy-bulma',
-    version='0.1.1',
+    version='0.1.2',
     description='Django application to add \'django-crispy-forms\' layout objects for Bulma.io',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
